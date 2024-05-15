788d288f7171ef2f4c32290ef9e5c1d1d99fb88b
HashirAKB <ahmedhashir96@gmail.com>
2024-05-15 19:38:13 +0530

    Completed VideoGrid Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { VideoCard } from "@/components/VideoCard";
+import { VideoGrid } from "@/components/VideoGrid";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-    <VideoCard title={"Entri Elevate “PORUL” | EP3 | WEBSERIES | KARIKKU"} channelName={"Karikku"} views={"1.7M"} uploadDate={"1 day ago"} logo={"/kariku_log.jpg"} thumbnail={"/porul_ep_3.jpg"}/>
+      <VideoGrid/>
     </>
   );
 }
