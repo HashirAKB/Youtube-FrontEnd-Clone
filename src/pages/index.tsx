716c5daa0fc984f144ca70342e77a67c465c521d
HashirAKB <ahmedhashir96@gmail.com>
2024-05-15 20:10:09 +0530

    Completed Search Bar.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import { VideoCard } from "@/components/VideoCard";
 import { VideoGrid } from "@/components/VideoGrid";
+import { AppBar } from "@/components/AppBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <AppBar/>
       <VideoGrid/>
     </>
   );
